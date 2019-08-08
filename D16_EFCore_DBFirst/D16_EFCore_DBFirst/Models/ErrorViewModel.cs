930b2cb871a2a8a2d9d924287de886ad42d158fe
Hien Luong <hyhien@gmail.com>
2019-08-08 18:51:44 +0700

    D16 - EFCore - DB First

---
+++ D16_EFCore_DBFirst/D16_EFCore_DBFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace D16_EFCore_DBFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
