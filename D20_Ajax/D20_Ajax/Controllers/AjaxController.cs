d30c906de30f0ded0dc2bb697b42dcd6aa5e07d2
Hien Luong <hyhien@gmail.com>
2019-08-17 19:05:26 +0700

    Search hàng hóa

---
+++ D20_Ajax/D20_Ajax/Controllers/AjaxController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCore_DBFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace D20_Ajax.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly MyeStoreContext _context;
+        public AjaxController(MyeStoreContext db)
+        {
+            _context = db;
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessSearch(string keyword)
+        {
+            var data = _context.HangHoa
+                .Include(hh => hh.MaLoaiNavigation)
+                .Include(hh => hh.MaNccNavigation)
+                .Where(hh => hh.TenHh.ToLower().Contains(keyword));
+            return PartialView(data);
+        }
+    }
+}
